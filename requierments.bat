11c88eaf4424265c0947fa24cc5dcfd061969009
morgan <mp390@humboldt.edu>
2021-03-15 21:32:00 -0700

    requierments finished

--- requierments.bat
+++ requierments.bat
@@ -3,11 +3,13 @@ pip install requests
 
 
 
-these will be ttorials i used to help me with this project
+
+these will be tutorials i used to help me with this project
 #using import requests in a GUI (how to put stuff in a label to)
 https://www.youtube.com/watch?v=YXPyB4XeYLA
 
 #showed setText & class set up again
 https://www.youtube.com/watch?v=-2uyzAqefyE&list=PLzMcBGfZo4-lB8MZfHPLTEHO9zJDDLpYj&index=2
 
-#showed
+#adam showed me this temperatureLoc = str(forcast['main']['temp'])
+use this to grab from dictionary in a class. 
